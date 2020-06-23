1cb9eda26d1eaf697c98eeea0fb8d3ffa957443c
karanhere3962 <karanhere3962@gmail.com>
2020-06-24 00:38:10 +0530

    adding build

---
+++ drag_drop/build/precache-manifest.46dc084ed4675ebf00573fc810faf218.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8db3de8814197df58d69b7a53da224f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56b6de97d04afd4d50e3",
+    "url": "/static/css/main.81e96baf.chunk.css"
+  },
+  {
+    "revision": "40bced8f3584e2a10e36",
+    "url": "/static/js/2.8e88c38c.chunk.js"
+  },
+  {
+    "revision": "436f23c45f716d0ee89b73f3096ad986",
+    "url": "/static/js/2.8e88c38c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56b6de97d04afd4d50e3",
+    "url": "/static/js/main.84c7b741.chunk.js"
+  },
+  {
+    "revision": "19442f397a60fe1d5e7f",
+    "url": "/static/js/runtime-main.bf5ef4bd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
